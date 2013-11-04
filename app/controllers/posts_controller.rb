f9fe4ad4cb017231f0fbae62571087ea035e46f2
Pablo Salazar <pabloa.salazarg@gmail.com>
2013-11-04 18:52:32 -0300

    Se agregan las funciones CRUD

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -9,14 +9,38 @@ class PostsController < ApplicationController
 	end
 
 	def create
-  		@post = Post.new(post_params)
+  		@post = Post.new(params[:post].permit(:title, :text))
  
-  		@post.save
-  		redirect_to @post
+  		if @post.save
+    		redirect_to @post
+  		else
+    		render 'new'
+  		end
 	end
 
+	def edit
+  		@post = Post.find(params[:id])
+	end
+	
 	def show
   		@post = Post.find(params[:id])
+	end
+
+	def update
+  		@post = Post.find(params[:id])
+ 
+  		if @post.update(params[:post].permit(:title, :text))
+    		redirect_to @post
+  		else
+    		render 'edit'
+  		end
+	end
+
+	def destroy
+  		@post = Post.find(params[:id])
+  		@post.destroy
+ 
+  		redirect_to posts_path
 	end
  
 	private
