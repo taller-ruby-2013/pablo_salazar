f1a18948e048ae54545119b1508da092926895d3
Pablo Salazar <pablosalazar@pablos-air.iptv\.microsoft\.com>
2013-12-15 13:51:31 -0300

    Poniendome al dia.. clase 7

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,17 @@
 Blog::Application.routes.draw do
+  
+
+  devise_for :users
+  resources :posts do
+    resources :comments
+  end
+
   get "welcome/index"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
   root 'welcome#index'
-
-  resources :posts do
-    resources :comments
-  end
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
